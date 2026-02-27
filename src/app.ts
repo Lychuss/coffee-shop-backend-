a789ffd7afb4f910f22be2292b0d21a46a1800b0
Raphae San Juan <raphaelsanjuan6@gmail.com>
2026-02-27 14:46:39 +0800

    I dont know i just change the code back into its original to see if it works

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,13 @@ app.use(cors({
 }));
 
 app.use(cookieParser());
-app.use(express.json());
+app.use((req, res, next) => {
+  if (["POST", "PUT", "PATCH"].includes(req.method)) {
+    express.json()(req, res, next);
+  } else {
+    next();
+  }
+});
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/health", (req: Request, res: Response) => {
