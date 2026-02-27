e34dcd7184bf8198b2eff7786baa0a5ff4103be7
Raphae San Juan <raphaelsanjuan6@gmail.com>
2026-02-28 02:44:38 +0800

    create a controller for getting the bills of the user

--- src/controllers/add-cart.controller.ts
+++ src/controllers/add-cart.controller.ts
@@ -1,7 +1,7 @@
 import type { Request, Response } from "express";
 import type { CartItem } from "../models/cart.interface";
 import { returnPayload } from "../services/token.service";
-import { add_cart_items, get_total_items, display_products_cart } from "../repository/addcart.repository";
+import { add_cart_items, get_total_items, display_products_cart, display_product_bills } from "../repository/addcart.repository";
 import { v4 as uuidv4 } from "uuid";
 import type { User } from "../models/user.interface";
 
@@ -32,10 +32,28 @@ export const display_cart = async (req: Request, res: Response) => {
     if(!token || !payLoad) return res.status(404).json({ message: 'Invalid token! must have a token', success: false });
 
     try {
-        const query = await display_products_cart(payLoad.userId);
+        const query_cart = await display_products_cart(payLoad.userId);
+        const query_bills = await display_product_bills(payLoad.userId);
+        const data = query_cart.rows;
+        const bills = query_bills.rows;
+        console.log(data); 
+        return res.status(200).json({ message: 'Successfully get all the product cart', data: data, success: true, bills: bills });
+    } catch(err) {
+        console.log(err);
+        return res.status(500).json({ message: 'Internal server error!', success: false });
+    }
+}
+
+export const display_bills = async (req: Request, res: Response) => {
+    const token = req.cookies.token;
+    const payLoad: User | null = returnPayload(token);
+    if(!token || !payLoad) return res.status(404).json({ message: 'Invalid token! must have a token', success: false });
+
+    try {
+        const query = await display_product_bills(payLoad.userId);
         const data = query.rows;
         console.log(data); 
-        return res.status(200).json({ message: 'Successfully get all the product cart', data: data, success: true });
+        return res.status(200).json({ message: 'Successfully get all the product bills', data: data, success: true });
     } catch(err) {
         console.log(err);
         return res.status(500).json({ message: 'Internal server error!', success: false });
--- src/routes/addcart.routes.ts
+++ src/routes/addcart.routes.ts
@@ -1,11 +1,12 @@
 import express from 'express';
-import { add_cart, display_cart } from '../controllers/add-cart.controller';
+import { add_cart, display_cart, display_bills } from '../controllers/add-cart.controller';
 import { authenticated } from '../middlewares/authorization.middlewares';
 
 const cartRouter = express.Router();
 
 cartRouter.post('/product/add-cart', authenticated, add_cart);
 cartRouter.get('/product/all-user-cart', authenticated, display_cart);
+cartRouter.get('/product/all-user-bills', authenticated, display_bills);
 
 export default cartRouter;
 
